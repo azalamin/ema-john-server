b27915f48025c96ffe68d7180e78eb716357e561
Al-amin Sheikh <azalamins@gmail.com>
2022-04-24 01:00:43 +0600

    created server and showed first output

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 // use middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("John is running and waiting for ema")
+})
+
+app.listen(port, () => {
+    console.log('Listening to the port', port)
+})
