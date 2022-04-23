a70b7de5fdab98d86d23102fcd85803edb60472c
Al-amin Sheikh <azalamins@gmail.com>
2022-04-24 00:53:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// use middleware
+app.use(cors())
+app.use(express.json())
